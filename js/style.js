b2aaa4840eaf0cf38bdd0697127e7495db72398d
rhui <rhui@192.168.3.180>
2017-03-17 11:50:19 +0800

    htc

--- js/style.js
+++ js/style.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() { 
-//搜索框
+	//搜索框
 	$("#seek_choice").hover(function(){
 		$("#choice_con").show();
 	},
@@ -11,5 +11,17 @@ $(document).ready(function() {
 		$("#seek_choice a samp").html($(this).html());
 		$("#choice_con").hide();
 	});
-//导航分类
+	//导航分类
+	$("#nav_all").click(function(){
+		if($(this).find("a").attr("name")!="home"){
+			$("#nav_all_one").slideToggle(0);
+		}
+	});
+	
+	$("#nav_all_one li").hover(function(){
+		$(this).find(".nav_class").show();
+	},
+	function(){
+		$(this).find(".nav_class").hide();
+	});
 }); 
