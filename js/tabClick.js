f020160634d8c3b65821be5cd729664a6a91f4a8
Administrator <Administrator@lzpx1>
2017-03-24 18:09:33 +0800

    lzp-03-24

--- js/tabClick.js
+++ js/tabClick.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 	//tab切换
 	$(".right_con .tab_nav span").on('click',function(){
@@ -7,4 +6,26 @@ $(document).ready(function() {
 		$(this).addClass('isclick').siblings().removeClass('isclick');
 	});
 	//简介截取字段
+});
+$(function() {
+	var tabs = function(el) {
+		this.el = el || {};
+		var links = this.el.find('.link');
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);	
+	}
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next(),
+			bInfo = "",
+			isRoot=$this.hasClass('rootlink');
+		$next.slideToggle(function(){
+			$(this).find('.submenu').css('display','none').parent().removeClass('open');
+		});
+		$this.parent().toggleClass('open');
+		if (!e.data.multiple&&isRoot) {
+			$this.parent().siblings().find('.submenu').slideUp().parent().removeClass('open');
+		};
+	}	
+	var accordion = new Accordion($('#accordion'), false);
 });
